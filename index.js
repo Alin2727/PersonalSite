313ee684d8e0febb7d27e0c70122c0d1f9f07fa0
Alin <alinbotofei99@yahoo.com>
2023-06-03 17:08:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+ 
+    // Fakes the loading setting a timeout
+      setTimeout(function() {
+          $('body').addClass('loaded');
+      }, 1500);
+   
+  });
+
+// Navbar on scroll
+  window.onscroll = function() {scrollFunction()};
+ 
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      
+      document.getElementById("navbar").style.background = "none"; // on scroll
+    } else {
+     
+      document.getElementById("navbar").style.background = "#EA5455"; // initial
+    }
+  }
