143a63d54838e9bdd6c1bb8f86ff681ac8725ca6
Alin <alinbotofei99@yahoo.com>
2023-06-04 22:20:25 +0300

    Add new features

--- index.js
+++ index.js
@@ -7,15 +7,38 @@ $(document).ready(function() {
    
   });
 
-// Navbar on scroll
-  // window.onscroll = function() {scrollFunction()};
- 
-  // function scrollFunction() {
-  //   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      
-  //     document.getElementById("navbar").style.background = "none"; // on scroll
-  //   } else {
-     
-  //     document.getElementById("navbar").style.background = "#EA5455"; // initial
-  //   }
-  // }
+  // Show & hidden animation
+
+
+  const observerLeft = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if(entry.isIntersecting) {
+        entry.target.classList.add('show-left');
+      }
+      // } else{
+      //   entry.target.classList.remove('show');
+      // }
+    });
+  });
+  
+    const hiddenElementsLeft = document.querySelectorAll('.hidden-left');
+    hiddenElementsLeft.forEach((el) => observerLeft.observe(el));
+  
+
+    const observerRight = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+          entry.target.classList.add('show-right');
+        }
+        // } else{
+        //   entry.target.classList.remove('show');
+        // }
+      });
+    });
+    
+      const hiddenElementsRight = document.querySelectorAll('.hidden-right');
+      hiddenElementsRight.forEach((el) => observerRight.observe(el));
+    
+  
